9014eb0499870017758559a260a3045cf3c80ea5
sirlocust <jua56yu@hotmail.com>
2021-04-07 23:50:41 -0500

    Setup project and Database

---
+++ src/main/java/com/example/devsirlocust/blog/model/Post.java
@@ -0,0 +1,15 @@
+package com.example.devsirlocust.blog.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table
+public class Post {
+
+}
